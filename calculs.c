544aac92aa2a94207b0da793a73a53bdf82a8820
Gaetan CADIOU <gcadiou@e1r6p1.42.fr>
2017-12-12 14:16:27 +0100

    little save because need to reset local data

--- calculs.c
+++ calculs.c
@@ -6,7 +6,7 @@
 /*   By: gcadiou <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 12:52:33 by gcadiou           #+#    #+#             */
-/*   Updated: 2017/12/12 14:06:05 by gcadiou          ###   ########.fr       */
+/*   Updated: 2017/12/12 14:15:54 by gcadiou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,14 @@ void		calculs_elem_size(t_elem *elem, t_infos *infos)
 		x = ft_strlen(elem->name);
 		if (infos->max_size_elem > x)
 			infos->max_size_elem = x;
-		infos->max_size_all += (x + SPACE_ELEM);
+		infos->max_size_all += x;
 		elem = elem->next;
 	}
 }
 void		calculs_win(t_infos *infos)
 {
-
+	ioctl(STDIN_FILENO, TIOCGWINSZ, infos->ws);
+	infos->nb_per_line = infos->ws.col / 
 }
 
 void		calculs(t_elem *entries, t_infos *infos)
